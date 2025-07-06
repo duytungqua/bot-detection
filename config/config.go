04ca23040e54ac3735c83da3d33e924fc7c170b0
duytungqua <quangduy.hcmut@gmail.com>
2025-07-06 23:23:13 +0700

    feat: update config kafka

--- config/config.go
+++ config/config.go
@@ -1,6 +1,16 @@
 package config
 
+import (
+	"github.com/go-playground/locales/vo"
+	"github.com/spf13/viper"
+)
+
 type Config struct {
+	Kafka struct {
+		Brokers []string
+		Topic   string
+		GroupID string
+	}
 	API struct {
 		Endpoint string
 	}
@@ -9,6 +19,22 @@ var Cfg Config
 
 
 func Load() Config{
+	// Load configuration from a file or environment variables
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic("Failed to read configuration: " + err.Error())
+
+	}
+	err := viper.Unmarshal(&Cfg)
+	if err != nil {
+		panic("Failed to unmarshal configuration: " + err.Error())
+	}
+	
 	Cfg.API.Endpoint = "https://api.example.com"
 	return Cfg	
 
